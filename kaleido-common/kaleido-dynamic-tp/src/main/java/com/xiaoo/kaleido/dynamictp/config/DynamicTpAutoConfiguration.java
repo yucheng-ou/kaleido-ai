aead60716cc47b1d7ad8de7d99b2abdb9378d969
ouyucheng <yuchengo@sdust.edu.cn>
2026-01-15 20:17:46 +0800

    集成dynamic-tp

---
+++ kaleido-common/kaleido-dynamic-tp/src/main/java/com/xiaoo/kaleido/dynamictp/config/DynamicTpAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.xiaoo.kaleido.dynamictp.config;
+
+import org.dromara.dynamictp.spring.annotation.EnableDynamicTp;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDynamicTp
+public class DynamicTpAutoConfiguration {
+}
