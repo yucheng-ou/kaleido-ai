6b410df95974e0d57928ddcc141952860898e222
ouyucheng <yuchengo@sdust.edu.cn>
2026-01-29 20:58:04 +0800

    新增mcp服务

---
+++ kaleido-biz/kaleido-mcp/src/main/java/com/xiaoo/kaleido/mcp/config/McpConfig.java
@@ -0,0 +1,16 @@
+package com.xiaoo.kaleido.mcp.config;
+
+import com.xiaoo.kaleido.mcp.tool.WeatherService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class McpConfig {
+
+//    @Bean
+//    public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+//        return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+//    }
+}
