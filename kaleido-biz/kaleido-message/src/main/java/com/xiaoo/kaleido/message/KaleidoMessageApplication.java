1f14b14dc0646e8f4449301bca92af52545387cb
ouyucheng <yuchengo@sdust.edu.cn>
2026-02-07 14:22:46 +0800

    新增message服务

--- kaleido-biz/kaleido-message/src/main/java/com/xiaoo/kaleido/message/KaleidoMessageApplication.java
+++ kaleido-biz/kaleido-message/src/main/java/com/xiaoo/kaleido/message/KaleidoMessageApplication.java
@@ -1,16 +1,18 @@
-package com.xiaoo.kaleido.recommend;
+package com.xiaoo.kaleido.message;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author ouyucheng
+ * @date 2025/2/7
+ * @description
  */
-@SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.recommend")
+@SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.message")
 @EnableDubbo
-public class KaleidoRecommendApplication {
+public class KaleidoMessageApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KaleidoRecommendApplication.class, args);
+        SpringApplication.run(KaleidoMessageApplication.class, args);
     }
+
 }
--- kaleido-biz/kaleido-wardrobe/src/main/java/com/xiaoo/kaleido/wardrobe/KaleidoWardrobeApplication.java
+++ kaleido-biz/kaleido-wardrobe/src/main/java/com/xiaoo/kaleido/wardrobe/KaleidoWardrobeApplication.java
@@ -3,7 +3,11 @@ package com.xiaoo.kaleido.wardrobe;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author ouyucheng
+ * @date 2025/12/25
+ * @description
+ */
 @SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.wardrobe")
 @EnableDubbo
 public class KaleidoWardrobeApplication {
