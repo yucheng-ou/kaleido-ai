44550356a5e1d2d8a025dbda9765436d280b701b
ouyucheng <yuchengo@sdust.edu.cn>
2026-02-27 21:26:52 +0800

    初始化interview项目

---
+++ kaleido-biz/kaleido-interview/src/main/java/com/xiaoo/kaleido/interview/KaleidoInterviewApplication.java
@@ -0,0 +1,18 @@
+package com.xiaoo.kaleido.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AI面试助手服务启动类
+ *
+ * @author trae
+ * @date 2026/02/27
+ */
+@SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.interview")
+public class KaleidoInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaleidoInterviewApplication.class, args);
+    }
+}
