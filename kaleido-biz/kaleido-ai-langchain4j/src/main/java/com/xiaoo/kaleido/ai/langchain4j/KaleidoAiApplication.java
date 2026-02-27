390eb368fc07dfa5fb8d5be2562f8e4dfce06922
ouyucheng <yuchengo@sdust.edu.cn>
2026-02-27 20:45:26 +0800

    ai服务langchain4j版本初始化

--- kaleido-biz/kaleido-ai-langchain4j/src/main/java/com/xiaoo/kaleido/ai/langchain4j/KaleidoAiApplication.java
+++ kaleido-biz/kaleido-ai-langchain4j/src/main/java/com/xiaoo/kaleido/ai/langchain4j/KaleidoAiApplication.java
@@ -1,16 +1,16 @@
-package com.xiaoo.kaleido.ai;
+package com.xiaoo.kaleido.ai.langchain4j;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * ai服务启动类
+ * ai服务启动类（langchain4j版本）
  *
  * @author ouyucheng
  * @date 2025/1/27
  */
-@SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.ai")
+@SpringBootApplication(scanBasePackages = "com.xiaoo.kaleido.ai.langchain4j")
 @EnableDubbo
 public class KaleidoAiApplication {
 
